13cfee1bcc57f35648b8402386e8b0e046cc68c1
Michael J <30031129@cc.peralta.edu>
2024-09-17 18:39:26 -0700

    made guessing a little easier

--- GuessNumber.cpp
+++ GuessNumber.cpp
@@ -19,6 +19,12 @@ int main(){
         if(guess == secretNumber){
             cout << "You guessed it!";
             correctGuess = true;
+        }else{
+            if(guess > secretNumber){
+                cout << "Guess is too high. Guess Lower!" << endl;
+            }else{
+                cout << "Guess is too low. Guess Higher!" << endl;
+            }
         }
     }
     return 0;
