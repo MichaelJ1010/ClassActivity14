dce55e26c4f4d400c28c5b7b07344614c89fe2eb
Michael J <30031129@cc.peralta.edu>
2024-09-17 18:38:45 -0700

    continuous asking of the secret number

--- GuessNumber.cpp
+++ GuessNumber.cpp
@@ -11,6 +11,15 @@ int main(){
 
     srand(time(0));
     secretNumber = rand() % 10 + 1;
-    
+
+    cout << "I'm thinking of a number from 1 to 10. Try to guess my number!" << endl;
+
+    while(!correctGuess){
+        cin >> guess;
+        if(guess == secretNumber){
+            cout << "You guessed it!";
+            correctGuess = true;
+        }
+    }
     return 0;
 }
